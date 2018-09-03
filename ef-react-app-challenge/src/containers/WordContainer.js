7a1f83ac865054ef2ddb162ba98dd83058188acf
Gary Muir <garymuir17@gmail.com>
2018-09-03 21:35:27 +0100

    adds basic react set up

---
+++ ef-react-app-challenge/src/containers/WordContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class WordContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = null;
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default WordContainer;
